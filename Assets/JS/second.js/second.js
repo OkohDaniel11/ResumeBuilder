1a24e5f0bc0f3b5407110d0b47d29da722398893
OkohDaniel11 <129377762+OkohDaniel11@users.noreply.github.com>
2026-03-25 19:42:52 +0000

    initial commit

--- Assets/JS/second.js/second.js
+++ Assets/JS/second.js/second.js
@@ -1,9 +1,9 @@
 
 // 'yPercent: 100' starts the red div completely below the section
 // It then eases up to its '0' position (covering the top section)
-gsap.from(".bg-layer", {
+gsap.from(".ResumeTOP", {
   yPercent: 100,      // Start 100% below its natural position
-  duration: 1.5,      // Animation length
+  duration: 1.0,      // Animation length
   ease: "power2.out", // Smooth deceleration
   delay: 0.2          // Slight wait for the page to settle
 });
